71f89163feae849d64b88ca0d1362e3186fea9c0
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-19 21:35:20 -0300

    Algunos cambios

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-    const inputField = document.querySelector('.input');
+    const inputField = document.getElementById('custom-input');
 
     inputField.addEventListener('focus', () => {
-        inputField.style.borderColor = '#3273dc'; // Color más oscuro cuando está activo
+        inputField.classList.add('is-focused');
     });
-
+    
     inputField.addEventListener('blur', () => {
-        inputField.style.borderColor = '#ccc'; // Restaurar el color original
-    });
+        inputField.classList.remove('is-focused');
+    });    
 }
